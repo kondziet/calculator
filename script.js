87f37f94f05aeca6704875b6b6c50f0b2b898924
kondziet <konrad.dabrowski54@gmail.com>
2022-11-02 20:02:17 +0100

    Fix typo

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-const previousExpressionDisplay = document.querySelector('.upper-display')
-const currentExpressionDisplay = document.querySelector('.lower-display')
+const previousNumberDisplay = document.querySelector('.previous-number-display')
+const currentNumberDisplay = document.querySelector('.current-number-display')
 const clearBtn = document.querySelector('.clear')
 const deleteBtn = document.querySelector('.delete')
 const numbersBtns = document.querySelectorAll('.number')
 const operatorsBtns = document.querySelectorAll('.operator')
 
+let previousExpression

1e7bd6e8278b0af542fae38ae100d2f57cbfd6e2
kondziet <konrad.dabrowski54@gmail.com>
2022-11-02 20:10:16 +0100

    Add appendNumber function

--- script.js
+++ script.js
@@ -5,4 +5,24 @@ const deleteBtn = document.querySelector('.delete')
 const numbersBtns = document.querySelectorAll('.number')
 const operatorsBtns = document.querySelectorAll('.operator')
 
-let previousExpression
+let previousNumber = ""
+let operator = undefined
+let currentNumber = ""
+
+function appendNumber(digit) {
+    if (digit === "." && currentNumber.includes(".")) {
+        return
+    } else if (digit === "." && currentNumber === "") {
+        currentNumber += "0"
+        return
+    }
+
+    currentNumber += digit
+}
+
+numbersBtns.forEach(button => {
+    button.addEventListener("click", e => {
+        const digit = e.target.textContent
+        appendNumber(digit)
+    })
+})

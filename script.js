92b07d69eff552e4cb74d87ef44db797a2057a0d
kondziet <konrad.dabrowski54@gmail.com>
2022-11-02 20:00:40 +0100

    Change class names

---
+++ script.js
@@ -0,0 +1,7 @@
+const previousExpressionDisplay = document.querySelector('.upper-display')
+const currentExpressionDisplay = document.querySelector('.lower-display')
+const clearBtn = document.querySelector('.clear')
+const deleteBtn = document.querySelector('.delete')
+const numbersBtns = document.querySelectorAll('.number')
+const operatorsBtns = document.querySelectorAll('.operator')
+
